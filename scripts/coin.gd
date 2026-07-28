432c6a72e2c2fd6b684bc23eb6ba3c292c321ee6
rednaxelvon <rednaxel.von@gmail.com>
2026-07-28 17:38:22 +0300

    Delete scripts directory

--- scripts/coin.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _ready():
-	body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		GameManager.add_score(1)
-		queue_free()
